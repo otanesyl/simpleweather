6be8f1e16221a3cf31b70cc9c1771a479f759974
otanesyl <ferrarifomaul@hotmail.com>
2017-08-16 03:36:38 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/simpleweather/android/db/Province.java
@@ -0,0 +1,35 @@
+package com.simpleweather.android.db;
+
+/**
+ * Created by otanesyl on 2017/8/16.
+ */
+
+public class Province extends  DataSupport {
+    private int id;
+    private  String provinceName;
+    private  int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
